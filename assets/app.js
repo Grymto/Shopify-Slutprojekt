f76ace8a63da78568e59e28929f795e61a03ffcc
Simon Andersson <108905796+Grymto@users.noreply.github.com>
2024-03-18 22:04:33 +0100

    footer icon tags med javascript

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,7 @@
-
+var elements = document.querySelectorAll(".footer-block__details-content.rte");
+elements.forEach(function(element) {
+  var children = element.children;
+  for (var i = 0; i < children.length; i++) {
+    children[i].classList.add("icon" + (i + 1));
+  }
+});
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 //alert("Hello world");
+
+// Get all elements with the class .footer-block__details-content.rte
+var elements = document.querySelectorAll('.footer-block__details-content.rte');
+
+// Loop through each element
+elements.forEach(function(element) {
+    // Get all children of the current element
+    var children = element.children;
+
+    // Loop through each child element
+    for (var i = 0; i < children.length; i++) {
+        // Add a class to each child element with the format 'iconN' where N is the index + 1
+        children[i].classList.add('icon' + (i + 1));
+    }
+});
